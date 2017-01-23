06f3d38eaf9881846be55d002214141de9f6ecae
a8568730 <a8568730@hotmail.com>
2017-01-23 13:48:50 +0800

    從意傳移出組字

---
+++ src/網站/頁尾.jsx
@@ -0,0 +1,20 @@
+
+import React from 'react';
+import { Link } from 'react-router';
+
+export default class ToLam extends React.Component {
+  render () {
+    return (
+      <footer className='app footer inverted'>
+        <ul className='ui menu container inverted'>
+          <li className='item'>
+            <a href='https://github.com/i3thuan5/tsoo2ji7'>前端網站程式碼</a>
+          </li>
+          <li className='item'>
+            <a href='http://xn--jny.xn--v0qr21b.xn--kpry57d/'>相關專案</a>
+          </li>
+        </ul>
+      </footer>
+    );
+  }
+}
