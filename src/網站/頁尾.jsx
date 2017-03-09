13d71737816e5aff46c62177c7bacf29ee04b619
a8568730 <a8568730@hotmail.com>
2017-03-09 10:53:59 +0800

    在手機版會堆疊

--- src/網站/頁尾.jsx
+++ src/網站/頁尾.jsx
@@ -6,14 +6,10 @@ export default class ToLam extends React.Component {
   render () {
     return (
       <footer className='app footer inverted'>
-        <ul className='ui menu container inverted'>
-          <li className='item'>
-            <a href='https://github.com/i3thuan5/tsoo2ji7'>前端網站程式碼</a>
-          </li>
-          <li className='item'>
-            <a href='http://xn--jny.xn--v0qr21b.xn--kpry57d/'>相關專案</a>
-          </li>
-        </ul>
+        <div className='ui stackable inverted container menu'>
+          <a className='item' target='_blank' href='https://github.com/i3thuan5/tsoo2ji7'>Github</a>
+          <a className='item' target='_blank' href='http://xn--jny.xn--v0qr21b.xn--kpry57d/'>相關專案</a>
+        </div>
       </footer>
     );
   }
