32d56fb41b3d25bc3b3b9977c110cfe7a90b5ad5
薛丞宏 <ihcaoe@gmail.com>
2018-04-30 08:31:42 +0800

    修頁尾的連結

--- src/網站/頁尾.jsx
+++ src/網站/頁尾.jsx
@@ -7,8 +7,9 @@ export default class ToLam extends React.Component {
     return (
       <footer className='app footer inverted'>
         <div className='ui stackable inverted container menu'>
-          <a className='item' target='_blank' href='https://github.com/i3thuan5/tsoo2ji7'>Github</a>
-          <a className='item' target='_blank' href='http://xn--jny.xn--v0qr21b.xn--kpry57d/'>相關專案</a>
+          <a className='item' target='_blank' href='https://xn--v0qr21b.xn--kpry57d/'>意傳科技</a>
+          <a className='item' target='_blank' href='https://github.com/i3thuan5/tsoo2ji7'>前端專案</a>
+          <a className='item' target='_blank' href='https://github.com/i3thuan5/han3_ji7_tsoo1_kian3'>後端專案</a>
         </div>
       </footer>
     );
